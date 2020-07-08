6114ccb8c52a425d8596dd67aff53846c02291e6
Andy Adams <andrewdcadams@gmail.com>
2020-07-08 18:06:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creater(nameArray)
+  badgeArray = []
+  nameArray.each do |name|
+    badgeArray.push("Hello, my name is #{name}.")
+  end
+  return badgeArray
+end
